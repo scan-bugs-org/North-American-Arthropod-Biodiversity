5b612152b1dd908a759a912e90369a5a6acd591a
Nicolas J. Dowdy <34909248+njdowdy@users.noreply.github.com>
2019-12-17 21:12:35 -0500

    Updated after finding some filtering errors.

--- gbif_stats_scripts.sql
+++ gbif_stats_scripts.sql
@@ -26,53 +26,53 @@
 -- )
 -- "class", "order", "family" headers spelling altered because these can be reserved words in certain programming languages
 
--- get "taxonomicstatus" levels
-SELECT taxonomicstatus,count(*) from public.gbif_na_only
+-- "taxonomicstatus" = accepted names only
+SELECT taxonomicstatus,count(*) from public.gbif_na_only_updated
 group by taxonomicstatus
 
--- get "taxonrank" levels
-SELECT taxonrank,count(*) from public.gbif_na_only
+-- "taxonrank" = UNRANKED, species, subspecies, or ALL
+SELECT taxonrank,count(*) from public.gbif_na_only_updated
 group by taxonrank
 
 -- sheet1: count of orders,families, genera, and species for each class
-SELECT clazz,COUNT(DISTINCT ordder),COUNT(DISTINCT familyy),COUNT(DISTINCT genus),COUNT(*) from public.gbif_na_only WHERE taxonomicstatus='ACCEPTED' AND taxonrank='SPECIES'
+SELECT clazz,COUNT(DISTINCT ordder),COUNT(DISTINCT familyy),COUNT(DISTINCT genus),COUNT(*) from public.gbif_na_only_updated WHERE taxonomicstatus='ACCEPTED' AND taxonrank='SPECIES'
 GROUP BY clazz,taxonomicstatus,taxonrank;
 
 -- sheet2: count of families, genera, and species for each order
-SELECT clazz,ordder,COUNT(DISTINCT familyy),COUNT(DISTINCT genus),COUNT(*) from public.gbif_na_only WHERE taxonomicstatus='ACCEPTED' AND taxonrank='SPECIES'
+SELECT clazz,ordder,COUNT(DISTINCT familyy),COUNT(DISTINCT genus),COUNT(*) from public.gbif_na_only_updated WHERE taxonomicstatus='ACCEPTED' AND taxonrank='SPECIES' AND clazz NOT IN ('Merostomata','Trilobita')
 GROUP BY clazz,ordder,taxonomicstatus,taxonrank;
 
--- sheet3: count of genera, and species for each familyy
-SELECT clazz,ordder,familyy,COUNT(DISTINCT genus),COUNT(*) from public.gbif_na_only WHERE taxonomicstatus='ACCEPTED' AND taxonrank='SPECIES'
+--sheet3: count of genera, and species for each familyy
+SELECT clazz,ordder,familyy,COUNT(DISTINCT genus),COUNT(*) from public.gbif_na_only_updated WHERE taxonomicstatus='ACCEPTED' AND taxonrank='SPECIES' AND clazz NOT IN ('Merostomata','Trilobita')
 GROUP BY clazz,ordder,familyy,taxonomicstatus,taxonrank;
 
 -- sheet4: count of species for each genus
-SELECT clazz,ordder,familyy,genus,COUNT(*) from public.gbif_na_only WHERE taxonomicstatus='ACCEPTED' AND taxonrank='SPECIES'
+SELECT clazz,ordder,familyy,genus,COUNT(*) from public.gbif_na_only_updated WHERE taxonomicstatus='ACCEPTED' AND taxonrank='SPECIES' AND clazz NOT IN ('Merostomata','Trilobita')
 GROUP BY clazz,ordder,familyy,genus,taxonomicstatus,taxonrank;
 
 -- sheet5: count of classes, orders, families, genera, species, subspecies
 SELECT COUNT(DISTINCT clazz),COUNT(DISTINCT ordder),COUNT(DISTINCT familyy),COUNT(DISTINCT genus),(SELECT count(*) from (
-  select distinct genus,specificepithet from public.gbif_na_only
-) as t1),(SELECT count(*) from (
-  select distinct genus,specificepithet,infraspecificepithet from public.gbif_na_only
-) as t2) from public.gbif_na_only WHERE taxonomicstatus='ACCEPTED' AND taxonrank='SPECIES' AND clazz NOT IN ('Merostomata','Trilobita')
+  select distinct genus,species from public.gbif_na_only_updated
+) as t1) from public.gbif_na_only_updated WHERE taxonomicstatus='ACCEPTED' AND taxonrank='SPECIES' AND clazz NOT IN ('Merostomata','Trilobita')
 GROUP BY taxonomicstatus,taxonrank
 
 -- sheet6: how many UNRANKED classes, orders, families, genera, species
 -- e.g. "How many UNRANKED Lepidoptera have their highest identified rank at the level of Order?" (see "order_count" value for Lepidoptera)
-SELECT clazz,COUNT(DISTINCT ordder),COUNT(DISTINCT familyy),COUNT(DISTINCT genus),COUNT(*) from public.gbif_na_only
-where taxonrank='UNRANKED'
+SELECT clazz,COUNT(DISTINCT ordder),COUNT(DISTINCT familyy),COUNT(DISTINCT genus),COUNT(*) from public.gbif_na_only_updated
+WHERE taxonomicstatus='ACCEPTED' AND taxonrank='UNRANKED' AND clazz NOT IN ('Merostomata','Trilobita')
 GROUP BY clazz,taxonrank;
 
 -- sheet7: list all orders
-SELECT clazz,ordder from public.gbif_na_only
+SELECT clazz,ordder from public.gbif_na_only_updated
+WHERE taxonomicstatus='ACCEPTED' AND taxonrank='SPECIES' AND clazz NOT IN ('Merostomata','Trilobita')
 group by clazz,ordder;
 
 -- sheet8: list all families
-SELECT clazz,ordder,familyy from public.gbif_na_only
+SELECT clazz,ordder,familyy from public.gbif_na_only_updated
+WHERE taxonomicstatus='ACCEPTED' AND taxonrank='SPECIES' AND clazz NOT IN ('Merostomata','Trilobita')
 group by clazz,ordder,familyy;
 
 -- sheet9: list all groups containing UNRANKED taxa
-SELECT clazz,ordder,familyy from public.gbif_na_only
-where taxonrank='UNRANKED'
+SELECT clazz,ordder,familyy from public.gbif_na_only_updated
+WHERE taxonomicstatus='ACCEPTED' AND taxonrank='UNRANKED' AND clazz NOT IN ('Merostomata','Trilobita')
 group by clazz,ordder,familyy,taxonrank;
